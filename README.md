60252239949dbd8d8ec236b266f6127c94e4001f
Rei-x <wszyzdgo@gmail.com>
2021-11-07 19:57:40 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# Discord Alexa Bot
+
+Speech recognition bot for playing music, imitates Amazon Alexa.
+
+## Installation
+
+1. Clone this repo and cd into it
+
+```sh
+git clone https://github.com/Rei-x/discord-alexa
+cd discord-alexa
+```
+
+2. Create `config.json` file with bot token (check `config.json.example`)
+3. Install dependencies
+
+```sh
+npm install
+```
+
+## How to run it?
+
+```sh
+npm start
+```
+
+You should see "Ready!" in the console, if everything went correctly.
+Enter voice channel and type `?join` in text channel and bot should join.
+
+Try to say `Alexa play despacito` and bot will play music!
+
+To skip music say `Alexa skip`
---
+++ index.js
@@ -0,0 +1,65 @@
+const { Client, Intents } = require("discord.js");
+const { addSpeechEvent } = require("discord-speech-recognition");
+const { Player } = require("discord-music-player");
+const config = require("./config.json");
+
+const client = new Client({
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_VOICE_STATES,
+    Intents.FLAGS.GUILD_MESSAGES,
+  ],
+});
+
+addSpeechEvent(client);
+
+const player = new Player(client, {
+  leaveOnEnd: false,
+  leaveOnStop: false,
+});
+
+client.on("messageCreate", async (message) => {
+  const voiceChannel = message.member?.voice.channel;
+
+  if (voiceChannel && message.content === "?join") {
+    message.channel.send("Joining voice channel 🎤");
+    const queue = player.createQueue(message.guild.id);
+    await queue.join(voiceChannel);
+  }
+});
+
+client.on("speech", async (message) => {
+  console.log(`🎤 ${message.author.username}: ${message.content}`);
+
+  if (!message.content) return;
+  if (!message.content.toLowerCase().startsWith("alexa")) return;
+
+  const args = message.content
+    .toLowerCase()
+    .slice("alexa".length)
+    .trim()
+    .split(" ");
+  const command = args.shift();
+
+  const guildQueue = player.getQueue(message.guild.id);
+
+  if (command === "play") {
+    const musicTitle = args.join(" ");
+    console.log("📻 Playing music: ", musicTitle);
+
+    await guildQueue.play(musicTitle).catch((_) => {
+      if (!guildQueue) queue.stop();
+    });
+  }
+
+  if (command === "skip") {
+    console.log("Skipping track ->");
+    guildQueue.skip();
+  }
+});
+
+client.on("ready", () => {
+  console.log("Ready!");
+});
+
+client.login(config.TOKEN);
